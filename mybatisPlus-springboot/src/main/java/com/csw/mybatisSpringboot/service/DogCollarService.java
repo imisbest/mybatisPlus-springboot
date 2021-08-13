6342c89018a745557f18294e1b1b52cca96daf78
imisbest <48979536+imisbest@users.noreply.github.com>
2021-08-14 03:44:13 +0800

    Delete mybatisPlus-springboot directory

--- mybatisPlus-springboot/src/main/java/com/csw/mybatisSpringboot/service/DogCollarService.java
+++
@@ -1,7 +0,0 @@
-package com.csw.mybatisSpringboot.service;
-
-import java.util.Map;
-
-public interface DogCollarService {
-    Map<String, Object> selectCollarLeashByLast(int currentPage, int pageSize);
-}
